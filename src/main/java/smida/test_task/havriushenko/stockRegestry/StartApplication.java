159d94327012eb244ba20b2baeccbf40ef77979f
Mykyta <nik.gavrushenko@gmail.com>
2020-09-06 13:10:28 +0300

    Create Stock Registry Project Create gitignore Added dependencies in pom.xml

---
+++ src/main/java/smida/test_task/havriushenko/stockRegestry/StartApplication.java
@@ -0,0 +1,12 @@
+package smida.test_task.havriushenko.stockRegestry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartApplication {
+
+    public static void main(String...args){
+        SpringApplication.run(StartApplication.class,args);
+    }
+}
